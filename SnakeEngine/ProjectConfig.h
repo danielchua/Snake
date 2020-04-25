bd96023c9e7d8992fce8fd008142046311376e4e
Daniel Chua <daniel.chua@hotmail.com>
2020-04-25 08:20:26 +0200

    Added Version

--- SnakeEngine/ProjectConfig.h
+++ SnakeEngine/ProjectConfig.h
@@ -3,3 +3,7 @@
 /* Next steps
 	- Implement list of coordinates to for snake body
 */
+
+static const uint8_t VERSION_MAJOR = 1U;
+static const uint8_t VERSION_MINOR = 0U;
+static const uint8_t VERSION_REVISION = 0U;
